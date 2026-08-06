a2adae6ac5f970945a62dc0cabd19f0c8eae7eff
Developer <github01@example.com>
2026-08-05 22:04:51 -0400

    Created navbar component structure

---
+++ campus_resource_app/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+    return (
+        <nav>
+            <h1>Campus Flow</h1>
+
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Resources</a>
+                </li>
+                <li>
+                    <a href="#">Categories</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
