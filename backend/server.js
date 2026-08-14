d815627c215232a3e8f5aa2895c95ebb4b9cfcd2
Developer <github01@example.com>
2026-08-14 12:36:10 -0400

    configured CORS for frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pool from "./db/database.js";
 import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,12 @@ function isValidId(id) {
 
 app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    })
+);
 
 app.get("/health", (req, res) => {
     res.json({
